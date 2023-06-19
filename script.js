2f014b89b9459961c0ee2550434582229fe69fc3
HEEL PATEL <91087524+heelpatel01@users.noreply.github.com>
2023-06-20 01:01:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const chatbotToggler=document.querySelector(".chatbot-toggler");
 const CloseBtn=document.querySelector(".close-btn");
 
 let userMessage;
-const API_KEY="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";
+const API_KEY="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";
 const inputInitHeight=chatInput.scrollHeight;
 
 const createChatLi=(message,className)=>{
